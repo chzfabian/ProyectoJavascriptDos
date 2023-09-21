5a8de260403ffb9cfa00c1599c6b51e828900b0e
Fabian <cfacosta.16@gmail.com>
2023-09-21 16:49:51 -0300

    me va a dar algo!!

---
+++ cherisbuba.js
@@ -0,0 +1,121 @@
+export const productosTienda = [
+    // Te-Series// 
+{
+  id: 1,
+  pasillo: "ᴛᴇ-sᴇʀɪᴇs",
+  nombre: "ᴛʜᴀɪ ᴛᴇᴀ",
+  cantidad: 2,
+  precioUnitario: 5.99,
+},
+{
+  id: 2,
+  pasillo: "ᴛᴇ-sᴇʀɪᴇs",
+  nombre: "ɢʀᴇᴇɴ ᴛᴇᴀ",
+  cantidad: 3,
+  precioUnitario: 6.99,
+},
+{
+  id: 3,
+  pasillo: "ᴛᴇ-sᴇʀɪᴇs",
+  nombre: "ʟᴇᴍᴏɴ ᴛᴇᴀ",
+  cantidad: 1,
+  precioUnitario: 6.99,
+},
+    //Ice-Drink //   
+{
+  id: 4,
+  pasillo: "Ice-Drink",
+  nombre: "ᴄʜᴏᴄᴏʟᴀᴛᴇ",
+  cantidad: 5,
+  precioUnitario: 9.5,
+},
+{
+  id: 5,
+  pasillo: "Ice-Drink",
+  nombre: "ʀᴇᴅ ᴠᴇʟᴠᴇᴛ",
+  cantidad: 1,
+  precioUnitario: 9.5,
+},
+{
+  id: 6,
+  pasillo: "Ice-Drink",
+  nombre: "ᴍɪʟᴏ",
+  cantidad: 3,
+  precioUnitario: 9.99,
+},
+    //ᴄᴏғғᴇ-sᴇʀɪᴇs //  
+{
+  id: 7,
+  pasillo: "ᴄᴏғғᴇ-sᴇʀɪᴇs",
+  nombre:  "ᴋᴏᴘᴀᴊᴀ",
+  cantidad: 2,
+  precioUnitario: 9.99,
+},
+{
+  id: 8,
+  pasillo: "ᴄᴏғғᴇ-sᴇʀɪᴇs",
+  nombre:  "ᴋᴏᴘɪ sᴜʀᴇɴ",
+  cantidad: 12,
+  precioUnitario: 10.99,
+},
+//ᴄʜᴏᴄᴏ-sᴇʀɪᴇs//
+{
+  id: 9,
+  pasillo: "ᴄʜᴏᴄᴏ-sᴇʀɪᴇs",
+  nombre: "ᴋᴏᴘɪ ᴍᴀᴋɪᴀᴛᴏ",
+  cantidad: 1,
+  precioUnitario: 11.99,
+},
+{
+id: 10,
+  pasillo: "ᴄʜᴏᴄᴏ-sᴇʀɪᴇs",
+  nombre: "ᴋᴏᴘɪ ᴍᴀᴋɪᴀᴛᴏxx",
+  cantidad: 1,
+  precioUnitario: 14.99,
+
+},
+    //ʙᴏʙᴀᴡᴀʀ//  
+{
+  id: 11,
+  pasillo: "ʙᴏʙᴀᴡᴀʀ",
+  nombre: "ʙᴏʙᴀ ᴛʜᴀɪ ᴛᴇᴀ",
+  cantidad: 2,
+  precioUnitario: 14.99,
+},
+{
+  id: 12,
+  pasillo: "ʙᴏʙᴀᴡᴀʀ",
+  nombre: "ʙᴏʙᴀ ᴘᴀʟᴀᴄɪᴏs ᴛᴇᴀ",
+  cantidad: 1,
+  precioUnitario: 11.99, 
+},
+{
+  id: 13,
+  pasillo: "ʙᴏʙᴀᴡᴀʀ",
+  nombre: "ʙᴏʙᴀ ᴊᴜɪᴄᴇ ᴅʀɪɴᴋ",
+  cantidad: 4,
+  precioUnitario: 9.99, 
+},
+{
+  id: 14,
+  pasillo: "ʙᴏʙᴀᴡᴀʀ", 
+  nombre: "ʙᴏʙᴀ sᴛᴀʀᴡᴀʀ ᴛᴇᴀ",
+  cantidad: 3,
+  precioUnitario: 9.99,
+},
+{
+  id: 15,
+  pasillo: "ʙᴏʙᴀᴡᴀʀ",
+  nombre:  "ʙᴏʙᴀ ᴄᴀᴍᴘᴇᴏɴᴇs ᴅᴇʟ ᴍᴜɴᴅᴏ",
+  cantidad: 2,
+  precioUnitario: 15.99,  
+},
+ //Topping//  
+{
+  id: 16,
+  pasillo: "ᴛᴏᴘᴘɪɴɢ",
+  nombre: "ᴄʜᴏᴄᴏ ᴄʀᴇᴀᴍ +𝟺ᴋ",
+  cantidad: 3,
+  precioUnitario: 2,
+}, 
+]; 
--- script.js
+++ script.js
@@ -1,126 +1,6 @@
-const productosTienda = [
-        // Te-Series//
-    {
-      id: 1,
-      pasillo: "ᴛᴇ-sᴇʀɪᴇs",
-      nombre: "ᴛʜᴀɪ ᴛᴇᴀ",
-      cantidad: 2,
-      precioUnitario: 5.99,
-    },
-    {
-      id: 2,
-      pasillo: "ᴛᴇ-sᴇʀɪᴇs",
-      nombre: "ɢʀᴇᴇɴ ᴛᴇᴀ",
-      cantidad: 3,
-      precioUnitario: 6.99,
-    },
-    {
-      id: 3,
-      pasillo: "ᴛᴇ-sᴇʀɪᴇs",
-      nombre: "ʟᴇᴍᴏɴ ᴛᴇᴀ",
-      cantidad: 1,
-      precioUnitario: 6.99,
-    },
-        //Ice-Drink //   
-    {
-      id: 4,
-      pasillo: "Ice-Drink",
-      nombre: "ᴄʜᴏᴄᴏʟᴀᴛᴇ",
-      cantidad: 5,
-      precioUnitario: 9.5,
-    },
-    {
-      id: 5,
-      pasillo: "Ice-Drink",
-      nombre: "ʀᴇᴅ ᴠᴇʟᴠᴇᴛ",
-      cantidad: 1,
-      precioUnitario: 9.5,
-    },
-    {
-      id: 6,
-      pasillo: "Ice-Drink",
-      nombre: "ᴍɪʟᴏ",
-      cantidad: 3,
-      precioUnitario: 9.99,
-    },
-        //ᴄᴏғғᴇ-sᴇʀɪᴇs //  
-    {
-      id: 7,
-      pasillo: "ᴄᴏғғᴇ-sᴇʀɪᴇs",
-      nombre:  "ᴋᴏᴘᴀᴊᴀ",
-      cantidad: 2,
-      precioUnitario: 9.99,
-    },
-    {
-      id: 8,
-      pasillo: "ᴄᴏғғᴇ-sᴇʀɪᴇs",
-      nombre:  "ᴋᴏᴘɪ sᴜʀᴇɴ",
-      cantidad: 12,
-      precioUnitario: 10.99,
-    },
-    //ᴄʜᴏᴄᴏ-sᴇʀɪᴇs//
-    {
-      id: 9,
-      pasillo: "ᴄʜᴏᴄᴏ-sᴇʀɪᴇs",
-      nombre: "ᴋᴏᴘɪ ᴍᴀᴋɪᴀᴛᴏ",
-      cantidad: 1,
-      precioUnitario: 11.99,
-    },
-    {
-    id: 10,
-      pasillo: "ᴄʜᴏᴄᴏ-sᴇʀɪᴇs",
-      nombre: "ᴋᴏᴘɪ ᴍᴀᴋɪᴀᴛᴏxx",
-      cantidad: 1,
-      precioUnitario: 14.99,
-
-    },
-        //ʙᴏʙᴀᴡᴀʀ//  
-    {
-      id: 11,
-      pasillo: "ʙᴏʙᴀᴡᴀʀ",
-      nombre: "ʙᴏʙᴀ ᴛʜᴀɪ ᴛᴇᴀ",
-      cantidad: 2,
-      precioUnitario: 14.99,
-    },
-    {
-      id: 12,
-      pasillo: "ʙᴏʙᴀᴡᴀʀ",
-      nombre: "ʙᴏʙᴀ ᴘᴀʟᴀᴄɪᴏs ᴛᴇᴀ",
-      cantidad: 1,
-      precioUnitario: 11.99, 
-    },
-    {
-      id: 13,
-      pasillo: "ʙᴏʙᴀᴡᴀʀ",
-      nombre: "ʙᴏʙᴀ ᴊᴜɪᴄᴇ ᴅʀɪɴᴋ",
-      cantidad: 4,
-      precioUnitario: 9.99, 
-    },
-    {
-      id: 14,
-      pasillo: "ʙᴏʙᴀᴡᴀʀ", 
-      nombre: "ʙᴏʙᴀ sᴛᴀʀᴡᴀʀ ᴛᴇᴀ",
-      cantidad: 3,
-      precioUnitario: 9.99,
-    },
-    {
-      id: 15,
-      pasillo: "ʙᴏʙᴀᴡᴀʀ",
-      nombre:  "ʙᴏʙᴀ ᴄᴀᴍᴘᴇᴏɴᴇs ᴅᴇʟ ᴍᴜɴᴅᴏ",
-      cantidad: 2,
-      precioUnitario: 15.99,  
-    },
-     //Topping//  
-    {
-      id: 16,
-      pasillo: "Topping",
-      nombre: "ᴄʜᴏᴄᴏ ᴄʀᴇᴀᴍ +𝟺ᴋ",
-      cantidad: 3,
-      precioUnitario: 2,
-    },
-  ];
+import {productosTienda} from "./cherisbuba" 
   let opcion;
-  alert("ʙɪᴇɴᴠᴇɴɪᴅᴏs ᴀ ᴄʜᴇʀɪs ʙᴜʙᴀ ☕");
+  alert("ʙɪᴇɴᴠᴇɴɪᴅᴏs ᴀ ᴄʜᴇʀɪs ʙᴜʙᴀ ☕"); 
   do {
     opcion = Number(
       prompt('ᴄʜᴇʀɪs ʙᴜʙʙᴀ ᴛɪᴇɴᴇ ʟᴏ ᴍᴇᴊᴏʀ ᴘᴀʀᴀ ᴛɪ: \n1: ᴛᴇ-sᴇʀɪᴇs. 🍵 \n2: ɪᴄᴇ-ᴅʀɪɴᴋ. 🥛 \n3: ᴄᴏғғᴇ-sᴇʀɪᴇs. ☕ \n4: ᴄʜᴏᴄᴏ-sᴇʀɪᴇs. 🥤 \n5: ʙᴏʙᴀᴡᴀʀ. 🍨 \n6: ᴛᴏᴘᴘɪɴɢ 🍫🍬'
@@ -147,7 +27,6 @@ const productosTienda = [
       alert(salida);
     }
   };
-  
   switch (opcion) {
     case 1:
         alert("ᴜsᴛᴇᴅ ᴅᴇᴄɪᴅɪᴏ ʟᴀ ᴄᴀᴛᴇɢᴏʀɪᴀ ᴅᴇᴛᴇ-sᴇʀɪᴇs ✨");
